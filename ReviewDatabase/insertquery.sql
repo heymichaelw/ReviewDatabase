50303fb699b80ac29d5e6c306fe49a642b6adf9b
heymichaelw <heymichaelw@gmail.com>
2017-01-25 10:03:07 -0500

    changed to allow to push

--- ReviewDatabase/insertquery.sql
+++ ReviewDatabase/insertquery.sql
@@ -1,5 +1,5 @@
 ﻿INSERT INTO Person
-VALUES (11, 'Barista', 'M', 'Greenville', '26');
+VALUES (11, 'Barista', 'M', 'Greenville', '25');
 
 INSERT INTO Review (Rating, Movie, Person, Id)
 VALUES (8, 1, 11, 17);
