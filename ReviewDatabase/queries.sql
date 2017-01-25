46c68d722c27460b84845d9e7997109bdfc1b0d1
heymichaelw <heymichaelw@gmail.com>
2017-01-25 09:58:40 -0500

    I really shouldv've committed more on this one!

---
+++ ReviewDatabase/queries.sql
@@ -0,0 +1,14 @@
+﻿SELECT Person.Age FROM Person
+INNER JOIN Review ON Person.Id = Review.Person
+INNER JOIN Movie ON Review.Movie = Movie.Id
+WHERE Movie.Title = 'Toy Story';
+
+SELECT Person.Occupation FROM Person
+WHERE Person.Age < 40;
+
+SELECT Movie.Title FROM Movie
+WHERE [IMDB Url] = NULL;
+
+SELECT Movie.Title FROM Movie
+INNER JOIN Review ON Movie.Id = Review.Movie
+WHERE Review.Rating < 2;
---
+++ insertquery.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO Person
+VALUES (11, 'Barista', 'M', 'Greenville', '26');
+
+INSERT INTO Review (Rating, Movie, Person, Id)
+VALUES (8, 1, 11, 17);
